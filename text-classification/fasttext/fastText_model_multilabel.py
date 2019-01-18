72894346c0a4198fa6b1d9272a7aff9907744ba1
=yisun <=>
2019-01-18 14:48:37 +0800

    text-classification

--- text-classification/fasttext/fastText_model_multilabel.py
+++ text-classification/fasttext/fastText_model_multilabel.py
@@ -1,8 +1,99 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-    脚本名:
-Created on 2019--
+    脚本名: fasttest 核心模型
+Created on 2019-01-17
 @author:David Yisun
 @group:data
 """
+
+import tensorflow as tf
+
+class fastTextB(object):
+    def __init__(self, label_size, learning_rate, batch_size, decay_steps, decay_rate, num_sampled, sentence_len, vocab_size,embed_size,is_training,max_label_per_example=5):
+        """init all hyperparameter here"""
+        # 1.set hyper-paramter
+        self.label_size = label_size  # e.g.1999
+        self.batch_size = batch_size  # batch size
+        self.num_sampled = num_sampled  # 样本数
+        self.sentence_len = sentence_len  # 样本长度
+        self.vocab_size = vocab_size  # 词表大小
+        self.embed_size = embed_size  # embedding 维度
+        self.is_training = is_training  # 是否训练
+        self.learning_rate = learning_rate  # 学习率
+        self.max_label_per_example = max_label_per_example  # 每个样本的label最大个数
+        self.initializer = tf.random_normal_initializer(stddev=0.1)  # 正太初始化
+
+        # 2.add placeholder (X,label)
+        self.sentence = tf.placeholder(tf.int32, [None, self.sentence_len], name="sentence")     # X
+        self.labels = tf.placeholder(tf.int64, [None, self.max_label_per_example], name="Labels")   # y [1,2,3,3,3]
+        self.labels_l1999 = tf.placeholder(tf.float32,[None, self.label_size])  # int64
+
+        # 3.set some variables
+        self.global_step = tf.Variable(0, trainable=False, name="Global_Step")   #
+        self.epoch_step = tf.Variable(0, trainable=False, name="Epoch_Step")
+        self.epoch_increment = tf.assign(self.epoch_step, tf.add(self.epoch_step, tf.constant(1)))
+        self.decay_steps, self.decay_rate = decay_steps, decay_rate
+        self.epoch_step = tf.Variable(0, trainable=False, name="Epoch_Step")
+
+        # 4.init weights 初始化权重
+        self.instantiate_weights()
+
+        # 5.main_graph: inference
+        self.logits = self.inference() # [None, self.label_size]
+
+        # 6.calculate loss
+        self.loss_val = self.loss()
+
+
+
+
+
+    def instantiate_weights(self):
+        """define all weights here"""
+        # embedding matrix
+        self.Embedding = tf.get_variable("Embedding", [self.vocab_size, self.embed_size],
+                                         initializer=self.initializer)
+        self.W = tf.get_variable("W", [self.embed_size, self.label_size], initializer=self.initializer)
+        self.b = tf.get_variable("b", [self.label_size])
+
+    def inference(self):
+        """main computation graph here: 1.embedding-->2.average-->3.linear classifier"""
+        # 1.get embedding of words in the sentence
+        sentence_embeddings = tf.nn.embedding_lookup(self.Embedding, self.sentence)  # [None,self.sentence_len,self.embed_size]
+
+        # 2.average vectors, to get representation of the sentence  映射出句子向量
+        self.sentence_embeddings = tf.reduce_mean(sentence_embeddings, axis=1)  # [None,self.embed_size]
+
+        # 3.linear classifier layer
+        logits = tf.matmul(self.sentence_embeddings, self.W) + self.b   # [None, self.label_size]==tf.matmul([None,self.embed_size],[self.embed_size,self.label_size])
+        return logits
+
+    def loss(self, l2_lambda=0.0001):
+
+        labels_multi_hot = self.labels_l1999  # [batch_size,label_size]
+        # sigmoid_cross_entropy_with_logits:Computes sigmoid cross entropy given `logits`.
+        # Measures the probability error in discrete classification tasks in which each class is independent and not mutually exclusive.
+        # For instance, one could perform multilabel classification where a picture can contain both an elephant and a dog at the same time.
+        loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_multi_hot, logits=self.logits)  # labels:[batch_size, label_size], logits:[batch_size, label_size]
+        loss = tf.reduce_mean(tf.reduce_sum(loss, axis=1))  # 算总的loss
+        print('loss:{0}'.format(loss))
+
+        # add regularization result in not converge
+        self.l2_losses = tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables() if 'bias' not in v.name]) * l2_lambda  # ？所有训练变量的l2 loss？
+        print("l2_losses:", self.l2_losses)
+        loss = loss + self.l2_losses
+        return loss
+
+    def train(self):
+        """based on the loss, use SGD to update parameter"""
+        learning_rate = tf.train.exponential_decay(self.learning_rate, self.global_step, self.decay_steps, self.decay_rate, staircase=True)  # 学习率衰减
+        # train_op = tf.contrib.layers.optimize_loss(self.loss_val, global_step=self.global_step,learning_rate=learning_rate, optimizer="Adam")
+        train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss=self.loss_val, global_step=self.global_step)
+        return train_op
+
+
+
+
+
+
--- text-classification/fasttext/fastText_train_multilabel.py
+++ text-classification/fasttext/fastText_train_multilabel.py
@@ -15,6 +15,10 @@ import os
 import word2vec
 import pickle
 import h5py
+from fastText_model_multilabel import fastTextB as fastText
+
+# 配置gpu资源
+os.environ['CUDA_VISIBLE_DEVICES'] = '6,7' # 使用 GPU 0
 
 
 #configuration
@@ -22,13 +26,225 @@ FLAGS=tf.app.flags.FLAGS
 
 
 tf.app.flags.DEFINE_string("cache_file_h5py", "/home/huzhiling/demo/data/text-classify/text-classification/data/ieee_zhihu_cup/data.h5","path of training/validation/test data.")  #../data/sample_multiple_label.txt 多标签文件
-tf.app.flags.DEFINE_string("cache_file_pickle", "/home/huzhiling/demo/data/text-classify/text-classification/data/ieee_zhihu_cup/vocab_label.pik","path of vocabulary and label files")   #../data/sample_multiple_label.txt
+tf.app.flags.DEFINE_string("cache_file_pickle", "/home/huzhiling/demo/data/text-classify/text-classification/data/ieee_zhihu_cup/vocab_label.pik", "path of vocabulary and label files")   #../data/sample_multiple_label.txt
 
 tf.app.flags.DEFINE_float("learning_rate", 0.001, "learning rate")
 tf.app.flags.DEFINE_integer("batch_size", 128, "Batch size for training/evaluating.")  # 512批处理的大小 32-->128
 tf.app.flags.DEFINE_integer("decay_steps", 20000, "how many steps before decay learning rate.")  # 批处理的大小 32-->128 多少steps后开始衰减
 tf.app.flags.DEFINE_float("decay_rate", 0.9, "Rate of decay for learning rate.")  # 0.5一次衰减多少  衰减比例
 
-tf.app.flags.DEFINE_integer("num_sampled",10,"number of noise sampling")  # 100  噪声取样
-tf.app.flags.DEFINE_string("ckpt_dir", "fast_text_checkpoint_multi/", "checkpoint location for the model")  # 模型保存位置
+tf.app.flags.DEFINE_integer("num_sampled", 10, "number of noise sampling")  # 100  噪声取样
+tf.app.flags.DEFINE_string("ckpt_dir", "./model", "checkpoint location for the model")  # 模型保存位置
+tf.app.flags.DEFINE_integer("sentence_len", 200, "max sentence length")  # 句子长度 多少个词
+tf.app.flags.DEFINE_integer("embed_size", 128, "embedding size")  # 128 embedding size
+tf.app.flags.DEFINE_boolean("is_training", True, "is traning.true:tranining,false:testing/inference")  # 训练还是推断
+tf.app.flags.DEFINE_integer("num_epochs", 25, "embedding size")  # epoch 数量
+tf.app.flags.DEFINE_integer("validate_every", 1, "Validate every validate_every epochs.")  # 每10轮做一次验证
+
+tf.app.flags.DEFINE_boolean("use_embedding", False, "whether to use embedding or not.")  # 是否使用外部embedding
+
+#1.load data(X:list of lint,y:int). 2.create session. 3.feed data. 4.training (5.validation) ,(6.prediction)
+
+
+def load_data(cache_file_h5py, cache_file_pickle):
+    """
+    load data from h5py and pickle cache files, which is generate by take step by step of pre-processing.ipynb
+    :param cache_file_h5py:
+    :param cache_file_pickle:
+    :return:
+    """
+    if not os.path.exists(cache_file_h5py) or not os.path.exists(cache_file_pickle):
+        raise RuntimeError("############################ERROR##############################\n. "
+                           "please download cache file, it include training data and vocabulary & labels. "
+                           "link can be found in README.md\n download zip file, unzip it, then put cache files as FLAGS."
+                           "cache_file_h5py and FLAGS.cache_file_pickle suggested location.")
+    print("INFO. cache file exists. going to load cache file")
+    f_data = h5py.File(cache_file_h5py, 'r')
+    print("f_data.keys:", list(f_data.keys()))
+    train_X=f_data['train_X']  # np.array(
+    print("train_X.shape:", train_X.shape)
+    train_Y=f_data['train_Y']  # np.array(
+    print("train_Y.shape:", train_Y.shape, ";")
+    vaild_X=f_data['vaild_X']  # np.array(
+    valid_Y=f_data['valid_Y']  # np.array(
+    test_X=f_data['test_X']  # np.array(
+    test_Y=f_data['test_Y']  # np.array(
+
+    word2index, label2index = None, None
+    with open(cache_file_pickle, 'rb') as data_f_pickle:
+        word2index, label2index = pickle.load(data_f_pickle)
+    print("INFO. cache file load successful...")
+    return word2index, label2index, train_X, train_Y, vaild_X, valid_Y, test_X, test_Y
+
+# 赋值外部embedding
+def assign_pretrained_word_embedding(sess, vocabulary_index2word, vocab_size, fast_text):
+    print("using pre-trained word emebedding.started...")
+    # word2vecc=word2vec.load('word_embedding.txt') #load vocab-vector fiel.word2vecc['w91874']
+    word2vec_model = word2vec.load('zhihu-word2vec-multilabel.bin-100', kind='bin')
+    word2vec_dict = {}
+    for word, vector in zip(word2vec_model.vocab, word2vec_model.vectors):
+        word2vec_dict[word] = vector
+    word_embedding_2dlist = [[]] * vocab_size  # create an empty word_embedding list.
+    word_embedding_2dlist[0] = np.zeros(FLAGS.embed_size)  # assign empty for first word:'PAD'
+    bound = np.sqrt(6.0) / np.sqrt(vocab_size)  # bound for random variables.
+    count_exist = 0
+    count_not_exist = 0
+    for i in range(1, vocab_size):  # loop each word
+        word = vocabulary_index2word[i]  # get a word
+        embedding = None
+        try:
+            embedding = word2vec_dict[word]  # try to get vector:it is an array.
+        except Exception:
+            embedding = None
+        if embedding is not None:  # the 'word' exist a embedding
+            word_embedding_2dlist[i] = embedding
+            count_exist = count_exist + 1  # assign array to this word.
+        else:  # no embedding for this word
+            word_embedding_2dlist[i] = np.random.uniform(-bound, bound, FLAGS.embed_size)
+            count_not_exist = count_not_exist + 1  # init a random value for the word.
+    word_embedding_final = np.array(word_embedding_2dlist)  # covert to 2d array.
+    word_embedding = tf.constant(word_embedding_final, dtype=tf.float32)  # convert to tensor
+    t_assign_embedding = tf.assign(fast_text.Embedding,
+                                   word_embedding)  # assign this value to our embedding variables of our model.
+    sess.run(t_assign_embedding)
+    print("word. exists embedding:", count_exist, " ;word not exist embedding:", count_not_exist)
+    print("using pre-trained word emebedding.ended...")
+
+# 在验证集上做验证，报告损失、精确度
+def do_eval(sess, fast_text, evalX, evalY, batch_size, vocabulary_index2word_label):  # evalY1999
+    evalX = evalX[0:3000]
+    evalY = evalY[0:3000]
+    number_examples, labels = evalX.shape
+    print("number_examples for validation:", number_examples)
+    eval_loss, eval_acc, eval_counter = 0.0, 0.0, 0
+    batch_size = 1
+    for start, end in zip(range(0, number_examples, batch_size), range(batch_size, number_examples,batch_size)):
+        evalY_batch = process_labels(evalY[start:end])
+        curr_eval_loss, logit = sess.run([fast_text.loss_val, fast_text.logits],  # curr_eval_acc-->fast_text.accuracy
+                                          feed_dict={fast_text.sentence: evalX[start:end], fast_text.labels_l1999: evalY[start:end]}) #,fast_text.labels_l1999:evalY1999[start:end]
+        #print("do_eval.logits_",logits_.shape)
+        label_list_top5 = get_label_using_logits(logit[0], vocabulary_index2word_label)
+        curr_eval_acc = calculate_accuracy(list(label_list_top5), evalY_batch[0], eval_counter) # evalY[start:end][0]
+        eval_loss, eval_counter, eval_acc = eval_loss+curr_eval_loss, eval_counter+1, eval_acc+curr_eval_acc
+
+    return eval_loss/float(eval_counter), eval_acc/float(eval_counter)
+
+def process_labels(trainY_batch, require_size=5, number=None):
+    """
+    限制label的输出维度  即 top N
+    process labels to get fixed size labels given a spense label
+    :param trainY_batch:
+    :return:
+    """
+    #print("###trainY_batch:",trainY_batch)
+    num_examples, _ = trainY_batch.shape
+    trainY_batch_result = np.zeros((num_examples, require_size), dtype=int)
+
+    for index in range(num_examples):
+        y_list_sparse = trainY_batch[index]
+        y_list_dense = [i for i, label in enumerate(y_list_sparse) if int(label) == 1]  # 选出每个样本所有标签 label向量中为1的
+        y_list = process_label_to_algin(y_list_dense, require_size=require_size)
+        trainY_batch_result[index]=y_list
+        if number is not None and number%30==0:
+            pass
+            #print("####0.y_list_sparse:",y_list_sparse)
+            #print("####1.y_list_dense:",y_list_dense)
+            #print("####2.y_list:",y_list) # 1.label_index: [315] ;2.y_list: [315, 315, 315, 315, 315] ;3.y_list: [0. 0. 0. ... 0. 0. 0.]
+    if number is not None and number % 30 == 0:
+        #print("###3trainY_batch_result:",trainY_batch_result)
+        pass
+    return trainY_batch_result
+
+def process_label_to_algin(ys_list,require_size=5):
+    """
+    given a list of labels, process it to fixed size('require_size')
+    :param ys_list: a list
+    :return: a list
+    """
+    ys_list_result = [0 for x in range(require_size)]
+    if len(ys_list) >= require_size:  # 超长
+        ys_list_result = ys_list[0:require_size]
+    else:  # 太短
+       if len(ys_list) == 1:
+           ys_list_result = [ys_list[0] for x in range(require_size)]
+       elif len(ys_list) == 2:
+           ys_list_result = [ys_list[0], ys_list[0], ys_list[0], ys_list[1], ys_list[1]]
+       elif len(ys_list) == 3:
+           ys_list_result = [ys_list[0], ys_list[0], ys_list[1], ys_list[1], ys_list[2]]
+       elif len(ys_list) == 4:
+           ys_list_result = [ys_list[0], ys_list[0], ys_list[1], ys_list[2], ys_list[3]]
+    return ys_list_result
+
+
+
+
+def main():
+    # 1.load_data
+    # word2index 词表
+    # label2index label id
+    # trainX 训练集  batch * sentence_len 句子长度
+
+    trainX, trainY, testX, testY = None, None, None, None
+    word2index, label2index, trainX, trainY, vaildX, vaildY, testX, testY = load_data(FLAGS.cache_file_h5py, FLAGS.cache_file_pickle)  #
+    index2label = {v: k for k, v in label2index.items()}  # label的id
+    vocab_size = len(word2index)   # 词表大小
+    print("cnn_model.vocab_size:", vocab_size)
+    num_classes = len(label2index)  # 类别大小
+    print("num_classes:", num_classes)
+    num_examples, FLAGS.sentence_len = trainX.shape  # 获取 input 的 shape
+    print("num_examples of training:", num_examples, "    sentence_len:", FLAGS.sentence_len)
+
+    # 2. create session
+    config = tf.ConfigProto()    # tf.ConfigProto一般用在创建session的时候。用来对session进行参数配置
+    config.gpu_options.allow_growth = True
+
+    with tf.Session(config=config) as sess:
+        # Instantiate Model
+        fast_text=fastText(num_classes, FLAGS.learning_rate, FLAGS.batch_size, FLAGS.decay_steps, FLAGS.decay_rate, FLAGS.num_sampled,
+                           FLAGS.sentence_len, vocab_size, FLAGS.embed_size, FLAGS.is_training)
+
+        # Initialize Save
+        saver = tf.train.Saver()
+        if os.path.exists(os.path.join(FLAGS.ckpt_dir, "checkpoint")):
+            print("Restoring Variables from Checkpoint")
+            saver.restore(sess, tf.train.latest_checkpoint(FLAGS.ckpt_dir))
+        else:
+            print('Initializing Variables')
+            sess.run(tf.global_variables_initializer())
+            if FLAGS.use_embedding:  # load pre-trained word embedding 是否用外部训练的embedding
+                vocabulary_index2word = {v: k for k, v in word2index.items()}
+                assign_pretrained_word_embedding(sess, vocabulary_index2word, vocab_size, fast_text)
+
+            curr_epoch = sess.run(fast_text.epoch_step)
+
+            # 3.feed data & training
+            number_of_training_data = len(trainX)  # 训练数据量
+            batch_size = FLAGS.batch_size
+            for epoch in range(curr_epoch, FLAGS.num_epochs):
+                loss, acc, counter = 0.0, 0.0, 0
+                for start, end in zip(range(0, number_of_training_data, batch_size), range(batch_size, number_of_training_data, batch_size)):
+                    curr_loss, current_l2_loss, _ = sess.run([fast_text.loss_val, fast_text.l2_losses, fast_text.train_op],
+                                                             feed_dict={fast_text.sentence: trainX[start: end],
+                                                                        fast_text.labels_l1999: trainY[start: end]})
+
+                    if epoch == 0 and counter == 0:
+                        print("trainX[start:end]:", trainX[start:end])  # 2d-array. each element slength is a 100.
+                        print("train_Y_batch:",
+                              trainY[start:end])  # a list,each element is a list.element:may be has 1,2,3,4,5 labels.
+
+                    loss, counter = loss + curr_loss, counter + 1  # acc+curr_acc loss累加 counter：统计每个epoch的batch数
+                    if counter % 50 == 0:
+                        print("Epoch %d\tBatch %d\tTrain Loss:%.3f\tL2 Loss:%.3f" % (epoch, counter, loss / float(counter), current_l2_loss))
+                        # \tTrain Accuracy:%.3f--->,acc/float(counter)
+
+                    if start % (1000 * FLAGS.batch_size) == 0:  # 每1000个batch 做一次验证
+                        eval_loss, eval_accuracy = do_eval(sess, fast_text, vaildX, vaildY, batch_size,
+                                                           index2label)  # testY1999,eval_acc
+
+
+
+
+
+
+
 
